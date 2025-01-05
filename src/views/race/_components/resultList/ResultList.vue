b956e18bc0452c1201b36e8375baa59fb1e64e92
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2025-01-05 03:02:06 +0300

    add skeleton component

--- src/views/race/_components/resultList/ResultList.vue
+++ src/views/race/_components/resultList/ResultList.vue
@@ -1,27 +1,35 @@
 <template>
-  <div v-for="(item, idx) in racingBoardStore.groupedResults" :key="idx">
-    <div class="flex justify-start bg-orange-200 px-4 rounded-md">
-      <RText as="p">{{ `${item.round}. Race ${item.distance}m` }}</RText>
+  <template v-if="groupedResults.length > 0">
+    <div v-for="(item, idx) in groupedResults" :key="idx">
+      <div class="flex justify-start bg-orange-200 px-4 rounded-md">
+        <RText as="p">{{ `${item.round}. Race ${item.distance}m` }}</RText>
+      </div>
+      <ul class="flex flex-col gap-2 p-0">
+        <li
+          class="items-center hidden gap-2 px-12 mb-2 text-sm font-normal rounded-lg lg:grid-rows-1 lg:grid grid-cols-4 text-r-secondary"
+        >
+          <div>Position</div>
+          <div>Name</div>
+          <div>Condition</div>
+          <div>Color</div>
+        </li>
+        <ResultListItem v-for="(result, idx) in item.results" :key="idx" :result="result" />
+      </ul>
     </div>
-    <ul class="flex flex-col gap-2 p-0">
-      <li
-        class="items-center hidden gap-2 px-12 mb-2 text-sm font-normal rounded-lg lg:grid-rows-1 lg:grid grid-cols-4 text-r-secondary"
-      >
-        <div>Position</div>
-        <div>Name</div>
-        <div>Condition</div>
-        <div>Color</div>
-      </li>
-      <ResultListItem v-for="(result, idx) in item.results" :key="idx" :result="result" />
-    </ul>
-  </div>
+  </template>
+  <RSkeleton v-else />
 </template>
 
 <script setup lang="ts">
 import ResultListItem from './ResultListItem.vue';
 import { useRacingBoardStore } from '@/stores/racingBoard';
+import { computed } from 'vue';
 
 const racingBoardStore = useRacingBoardStore();
+
+const groupedResults = computed(() => {
+  return racingBoardStore.groupedResults ?? [];
+});
 </script>
 
 <style scoped></style>
--- src/views/race/_components/schedule/ScheduleList.vue
+++ src/views/race/_components/schedule/ScheduleList.vue
@@ -1,25 +1,28 @@
 <template>
-  <div v-for="(schedule, idx) in racingBoardStore.raceScheduleList" :key="idx">
-    <div class="flex justify-start bg-orange-200 px-4 rounded-md">
-      <RText as="p">{{ `${schedule.round}. Race ${schedule.distance}m` }}</RText>
+  <template v-if="racingBoardStore.raceScheduleList.length > 0">
+    <div v-for="(schedule, idx) in racingBoardStore.raceScheduleList" :key="idx">
+      <div class="flex justify-start bg-orange-200 px-4 rounded-md">
+        <RText as="p">{{ `${schedule.round}. Race ${schedule.distance}m` }}</RText>
+      </div>
+      <ul class="flex flex-col gap-2 p-0">
+        <li
+          class="items-center hidden gap-2 px-12 mb-2 text-sm font-normal rounded-lg lg:grid-rows-1 lg:grid grid-cols-4 text-r-secondary"
+        >
+          <div>Position</div>
+          <div>Name</div>
+          <div>Condition</div>
+          <div>Color</div>
+        </li>
+        <ProgramListItem
+          v-for="(horse, idx) in schedule.horses"
+          :key="idx"
+          :position="idx + 1"
+          :horse="horse"
+        />
+      </ul>
     </div>
-    <ul class="flex flex-col gap-2 p-0">
-      <li
-        class="items-center hidden gap-2 px-12 mb-2 text-sm font-normal rounded-lg lg:grid-rows-1 lg:grid grid-cols-4 text-r-secondary"
-      >
-        <div>Position</div>
-        <div>Name</div>
-        <div>Condition</div>
-        <div>Color</div>
-      </li>
-      <ProgramListItem
-        v-for="(horse, idx) in schedule.horses"
-        :key="idx"
-        :position="idx + 1"
-        :horse="horse"
-      />
-    </ul>
-  </div>
+  </template>
+  <RSkeleton v-else />
 </template>
 
 <script setup lang="ts">
