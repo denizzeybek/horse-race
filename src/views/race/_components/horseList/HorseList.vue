21c7db72a97bf87d4c9acb956d2c56f2af493794
Deniz Zeybek <46136181+denizzeybek@users.noreply.github.com>
2025-01-05 15:46:04 +0300

    wrap horselist header with component

--- src/views/race/_components/horseList/HorseList.vue
+++ src/views/race/_components/horseList/HorseList.vue
@@ -1,22 +1,16 @@
 <template>
   <ul class="flex flex-col gap-2 p-0">
-    <li
-      class="items-center hidden gap-2 px-12 mb-2 text-sm font-normal rounded-lg lg:grid-rows-1 lg:grid grid-cols-4 text-r-secondary"
-    >
-      <div class="lg:col-span-2">Name</div>
-      <div>Condition</div>
-      <div>Color</div>
-    </li>
+    <HorseListHeader />
     <HorseListItem v-for="(horse, idx) in racingBoardStore.horseList" :key="idx" :horse="horse" />
   </ul>
 </template>
 
 <script setup lang="ts">
+import HorseListHeader from './HorseListHeader.vue';
 import HorseListItem from './HorseListItem.vue';
 import { useRacingBoardStore } from '@/stores/racingBoard';
 
 const racingBoardStore = useRacingBoardStore();
-
 </script>
 
 <style scoped></style>
